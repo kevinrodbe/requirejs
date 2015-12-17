fb11096052e6f3f614c8a8835a703fa1fd9aa6a0
kevinrodbe <kevin.rodriguez992@gmail.com>
2015-12-17 18:58:55 -0500

    3° Using the Text Plugin - init app w/ knockout

--- src/main.js
+++ src/main.js
@@ -1,9 +1,21 @@
 /* aun queremos abrir modulos como dependencias, así que no podemos usar el "define" metodo pq entonces este sería un regular modulo. */
+/*
 require(['task_manager','smokesignals'],function(taskManager,smokesignals){
 	'use strict';
-	/* como hemos puesto como dependecia al modulo "task_manager", podemos llamar al metodo createTask */
+	// como hemos puesto como dependecia al modulo "task_manager", podemos llamar al metodo createTask
 	var aTask=taskManager.createTask('My first task!');
 	console.log(smokesignals);
 	console.log(aTask);
+});
+define();
+*/
+
+require(['text!views/app.html', 'view_models/app', 'renderer'], function (appView, App, renderer) {
+    'use strict';
+
+    var container = document.body,
+        viewModel = new App();
+
+    renderer.render(container, appView, viewModel);
 });
 define();
